526e08eaaa50bb1b188d2ff1edf25c145bd477f1
Пользователь <@DESKTOP-PPHPJ8R>
2025-09-08 18:56:55 +0500

    Add project files.

---
+++ Tyuiu.ZakachurinIE.Sprint0.Task0.V0.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ZakachurinIE.Sprint0.Task0.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
