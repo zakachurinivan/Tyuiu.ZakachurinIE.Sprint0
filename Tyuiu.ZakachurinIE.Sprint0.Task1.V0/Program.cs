48764cd2d902af249097124d3ef9360df3a08e45
Иван Закачурин <vanekcarek43@gmail.com>
2025-09-08 19:29:15 +0500

    Task1

---
+++ Tyuiu.ZakachurinIE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World");
+string name = "Иван";
+int age = 18;
+Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+Console.ReadKey();
