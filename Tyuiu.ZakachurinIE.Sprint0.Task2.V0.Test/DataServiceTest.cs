af0c27977881c144efe0b79cbc641cacd6061903
Иван Закачурин <vanekcarek43@gmail.com>
2025-09-10 10:51:30 +0500

    Task2

---
+++ Tyuiu.ZakachurinIE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ZakachurinIE.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ZakachurinIE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChechGetMessageValid()
+        {
+            var name = "Иван";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Иван", res);
+        }
+    }
+}
---
+++ Tyuiu.ZakachurinIE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ZakachurinIE.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ZakachurinIE.Sprint0.Task2.V0;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Иван"));
+        Console.ReadKey();
+    }
+}
+
+
+
+
+
