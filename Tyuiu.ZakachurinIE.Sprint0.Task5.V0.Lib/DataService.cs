bfa5dc77c8f2aa613b16f10c3411496866628e14
Иван Закачурин <vanekcarek43@gmail.com>
2025-09-13 13:26:11 +0500

    Task5

---
+++ Tyuiu.ZakachurinIE.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyuiu.ZakachurinIE.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            if (b==0)
+            {
+                Console.WriteLine("Переменная b={0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Tyuiu.ZakachurinIE.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ZakachurinIE.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.ZakachurinIE.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
